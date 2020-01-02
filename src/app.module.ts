6e5afd6f2201b003e71ca4ec0a29d2a929dc98e8
Daniel Franco <dfranco@nearsoft.com>
2020-01-02 13:55:11 -0600

    Added base config for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,21 @@ import { ManufacturerModule } from './manufacturer/manufacturer.module';
 import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [CarModule, ManufacturerModule, OwnerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    CarModule,
+    ManufacturerModule,
+    OwnerModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as any,
+      host: process.env.DATABASE_HOST,
+      port: Number.parseInt(process.env.DATABASE_PORT, 10),
+      password: process.env.DATABASE_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
